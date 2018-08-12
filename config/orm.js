c20b55fd760752cd1d6943aac6abc10fd060cdec
Andrew Vala <38055092+awvala@users.noreply.github.com>
2018-08-12 16:55:18 -0500

    First working page with GET

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,42 @@
-var connection = require("./connection.js");
+var connection = require("./connection.js");;
+
+// Helper function for SQL syntax.
+// Let's say we want to pass 3 values into the mySQL query.
+// In order to write the query, we need 3 question marks.
+// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
 
 // NOTE TO SELF
 // The ?? signs are for swapping out table or column names
@@ -7,13 +45,14 @@ var connection = require("./connection.js");
 
 var orm = {
     selectAll: function (table, cb) {
-        var queryString = "SELECT * FROM ??";
-        connection.query(queryString, [table], function (err, result) {
+        var queryString = "SELECT * FROM ??;";
+        connection.query(queryString, table, function (err, result) {
             if (err) throw err;
-            console.log(result);
+            //console.log(result);
             cb(result);
         });
     },
+
     insertOne: function (table, colName, burgerName, cb) {
         var queryString = "INSERT INTO ?? (??) VALUES (?);";
         console.log(queryString);
@@ -23,6 +62,7 @@ var orm = {
             cb(result);
         });
     },
+
     updateOne: function (table, colName, devouredStatus, burgerID, cb) {
         var queryString = "UPDATE ?? SET ?? = ? WHERE id = ?;";
         console.log(queryString);
--- server.js
+++ server.js
@@ -3,40 +3,50 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 var mysql = require("mysql");
 var bodyParser = require("body-parser");
-var path = require("path");
 
 // Create an instance of the express app.
 var app = express();
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
+
+// MySQL DB Connection Information (remember to change this with our specific credentials)
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Tardis1011",
+  database: "burgers_db"
+});
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Static directory to be served
+app.use(express.static("/public"));
 
 // Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// MySQL DB Connection Information (remember to change this with our specific credentials)
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Tardis1011",
-    database: "burgers_db"
-  });
-  
-  // Initiate MySQL Connection.
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
+// Routes
+// =============================================================
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
 
+// Initiate MySQL Connection.
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+});
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
